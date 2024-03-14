36a6fe3ccf68c30e8c64904448240acb2122ded8
Anderson Contreira <anderson.contreira@gmail.com>
2024-03-14 12:07:46 -0300

    Projeto reorganizado, o exemplo 8 vamos usar um template pronto e aplicar programação. Projeto 9 será um desafio maior

---
+++ back-end/example-8-ecommerce-api-sqlite/lesson-1/README.md
@@ -0,0 +1,6 @@
+# Ecommerce
+
+
+# Referencies
+- https://themewagon.github.io/furni/blog.html
+- https://themewagon.com/themes/furni-online-store/
---
+++ back-end/example-8-ecommerce-api-sqlite/lesson-1/app.js
@@ -0,0 +1,125 @@
+import sqlite3 from 'sqlite3';
+import express from 'express';
+import { open } from 'sqlite';
+import bodyParser from 'body-parser';
+
+const db = await open({
+    filename: 'database.db',
+    driver: sqlite3.Database
+});
+
+sqlite3.verbose();
+
+export const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+
+async function createTodoItem(item) {
+    await db.open()
+
+    const result = await db.run(
+        'INSERT INTO todo (todo, completed, user_id) VALUES (?, ?, ?)',
+        [item.todo, item.completed ? 1 : 0, item.userId]
+    )
+
+    await db.close();
+    return result;
+}
+
+async function listTodos() {
+    await db.open()
+
+    let result = [];
+    const todoList = await db.all('SELECT * FROM todo');
+    if (todoList) {
+        result = todoList;
+    }
+
+    await db.close();
+    return result;
+}
+
+app.get('/', async (req, res, next) => {
+    res.render('index');
+});
+
+app.get('/about', async (req, res, next) => {
+    res.render('about');
+});
+
+app.get('/blog', async (req, res, next) => {
+    res.render('blog');
+});
+
+app.get('/cart', async (req, res, next) => {
+    res.render('cart');
+});
+
+app.get('/checkout', async (req, res, next) => {
+    res.render('checkout');
+});
+
+app.get('/contact', async (req, res, next) => {
+    res.render('contact');
+});
+
+app.get('/services', async (req, res, next) => {
+    res.render('services');
+});
+
+app.get('/shop', async (req, res, next) => {
+    res.render('shop');
+});
+
+app.get('/thankyou', async (req, res, next) => {
+    res.render('thankyou');
+});
+
+app.get('/product-detail', async (req, res, next) => {
+    res.render('product-detail');
+});
+
+
+app.post('/api/todo', async (req, res, next) => {
+
+    let item = {
+        todo: req.body?.todo || '',
+        completed: req.body?.completed || false,
+        userId: req.body?.userId || 1
+    };
+
+    let result = await createTodoItem(item);
+    let status = 201;
+    let jsonBody = {
+        "error": "Unable to save"
+    };
+    if (result) {
+        console.log(`Inserted with id: ${result.lastID}`);
+        jsonBody = {
+            id: result.lastID,
+            ...item
+        };
+    } else {
+        status = 500;
+    }
+
+    res.status(status).json(jsonBody);
+});
+
+app.get('/api/todo', async (req, res, next) => {
+
+    let result = await listTodos();
+
+    res.json(result);
+});
